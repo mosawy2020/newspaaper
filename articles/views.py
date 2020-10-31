c17fb57b78b8ea3e2529750c7c9e83cb6a3fff90
mosawy <sawyhawy@gmail.com>
2020-10-31 22:36:33 +0200

    first commit

---
+++ articles/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Article
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+# Create your views here.
+class ArticleListView(ListView):
+    model=Article
+    template_name = "article_list.html"
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name="article_detail.html"
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
+    model = Article
+    fields= ('title', 'body')
+    template_name = "article_edit.html"
+    login_url = "login"
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
+    login_url = "login"
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+class ArticleCreateView(LoginRequiredMixin,CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ("title", "body", "author")
+    login_url = "login"
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
